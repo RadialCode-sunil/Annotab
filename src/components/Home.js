1a4a1a7e0457de2e342b9a68df7bf02baa46a1c1
batesar007 <batesar017@gmail.com>
2023-04-11 22:58:19 +0530

    hd

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Resources from './Resources'
 import Team from './Team'
 import Hero from './Hero'
+import Process from './Process'
 
 const Home = () => {
     return (
         <div className='bg_darkblue'>
-            <Hero/>
+            <Hero />
             <Resources />
             <Team />
             <Process />
