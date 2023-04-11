875046f7280521aeade86f3e2ddd427e78521971
RadialCode-sunil <sunilbatesar0@gmail.com>
2023-04-11 19:27:42 +0530

    font size change media

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -5,23 +5,25 @@ const Resources = () => {
     return (
         <section>
             <Container className=''>
-                <div>
+                <div className=' d-flex flex-column flex-lg-row align-items-lg-center'>
                     <div>
                         <h2 className=' ff_Medium fw_400 fs_3xl white'>
                             Resources
                         </h2>
-                        <p className=' ff_Book fw_400 fs_sm white'>
+                        <p className=' ff_Book fw_400 fs_sm white w-75 '>
                             Magna scelerisque amet adipiscing diam ac justo. Elementum mattis aenean malesuada mauris eget platea id. Ullamcorper.
                         </p>
                     </div>
-                    <div>
-                        <p>
-                            Blogs
-                        </p>
-                        <p>
-                            Documentation
-                        </p>
-                    </div>
+                    <div className=' d-flex align-items-center'>
+                        <div className='Resources_Blogs px-3'>
+                            <p className='ff_Medium fw_400 fs_xl white'>
+                                Blogs
+                            </p></div>
+                        <div className='Resources_Documentation'>
+                            <p className='ff_Medium fw_400 fs_xl white ps-5 ms-4 '>
+                                Documentation
+                            </p>
+                        </div></div>
                 </div>
             </Container>
         </section>
