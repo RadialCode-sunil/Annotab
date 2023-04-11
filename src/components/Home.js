d7f6b8259d734b1144e8b357892e9f44c56f7402
sunil-170 <sunilkhokha015@gmail.com>
2023-04-11 23:03:33 +0530

    turpis

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from './NavBar'
 import Resources from './Resources'
 import Team from './Team'
+import { Turpis } from './Turpis'
 
 const Home = () => {
     return (
         <div className='bg_darkblue'>
             <NavBar />
             <Resources />
-            <Team/>
+            <Team />
+            <Turpis/>
         </div>
     )
 }
