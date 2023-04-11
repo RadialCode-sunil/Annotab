481491433cfdfb1cc38df4446600357463237796
RadialCode-sunil <sunilbatesar0@gmail.com>
2023-04-11 18:34:00 +0530

    add bootstrap link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './App.css';
 import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
---
+++ src/components/Resources.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Resources = () => {
+    return (
+        <section>
+            <Container className=''>
+                <div>
+                    <div>
+                        <h2>
+                            Resources
+                        </h2>
+                        <p>
+                            Magna scelerisque amet adipiscing diam ac justo. Elementum mattis aenean malesuada mauris eget platea id. Ullamcorper.
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            Blogs
+                        </p>
+                        <p>
+                            Documentation
+                        </p>
+                    </div>
+                </div>
+            </Container>
+        </section>
+    )
+}
+
+export default Resources
