38980e4de73fd00bd40e36efd421e42160382342
sunil-170 <sunilkhokha015@gmail.com>
2023-04-11 23:14:15 +0530

    fff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import Resources from './Resources'
 import Team from './Team'
-<<<<<<< HEAD
 import { Turpis } from './Turpis'
-=======
 import Hero from './Hero'
-<<<<<<< HEAD
->>>>>>> c90c80e6a8d3d6368369f6e146f61d20f4768dbd
-=======
 import Process from './Process'
-
->>>>>>> cb042dc01873995f16c9cca449388fe2aaad81c3
 
 const Home = () => {
     return (
@@ -18,15 +11,7 @@ const Home = () => {
             <Hero />
             <Resources />
             <Team />
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <Turpis/>
-=======
             <Process />
->>>>>>> c90c80e6a8d3d6368369f6e146f61d20f4768dbd
-=======
-            <Process/>
->>>>>>> cb042dc01873995f16c9cca449388fe2aaad81c3
         </div>
     )
 }
