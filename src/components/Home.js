2a60b15f8e2bbb1e9d23411eb4b87b49b9358720
batesar007 <batesar017@gmail.com>
2023-04-11 22:48:52 +0530

    Process

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from './NavBar'
 import Resources from './Resources'
 import Team from './Team'
+import Process from './Process'
 
 const Home = () => {
     return (
         <div className='bg_darkblue'>
             <NavBar />
             <Resources />
-            <Team/>
+            <Team />
+            <Process />
         </div>
     )
 }
