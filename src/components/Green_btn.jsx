70dbaa346d78d94f27939a4bfe61de5b70d5ef08
RadialCode-sunil <sunilbatesar0@gmail.com>
2023-04-12 11:05:03 +0530

    navbar

--- src/components/Green_btn.jsx
+++ src/components/Green_btn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Green_btn = (props) => {
     return (
         <div>
-            <a href="#" id={props.btn_id} className=' ff_Bold fs_md fw_400 butnnew butn d-inline-block'>{props.any_text} hello</a>
+            <a href="#" id={props.btn_id} className=' ff_Bold fs_md fw_400 butnnew butn d-inline-block'>{props.any_text}</a>
         </div>
     )
 }
