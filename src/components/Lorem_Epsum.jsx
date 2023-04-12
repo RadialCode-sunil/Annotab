bae92cab6fed338ef4d1bf6964f1f1e992cad36c
RadialCode-sunil <sunilbatesar0@gmail.com>
2023-04-12 22:44:04 +0530

    Lorem_Epsum

--- src/components/Lorem_Epsum.jsx
+++ src/components/Lorem_Epsum.jsx
@@ -11,28 +11,32 @@ const Lorem_Epsum = () => {
                     Lorem Epsum Dolar Sit emit
                 </h2>
 
-                <div className=' d-flex'>
+                <div className=' d-flex pt-5'>
                     <img className='' src={Manufacturing_Lorem_img1} alt="image" />
                     <div className=' ps-4'>
-                        <p className=' ff_Book fw_300 fs_sm white Lorem_Epsum_p_w'>
+                        <p className=' ff_Book fw_300 fs_sm white Lorem_Epsum_p_w341 Lorem_Epsum_Amet position-relative'>
                             .  Amet at ultricies adipiscing lectus sed vitae. Nullam quam facilisi.
                         </p>
-                        <p className=' ff_Book fw_300 fs_sm white Lorem_Epsum_p_w'>
+                        <p className=' ff_Book fw_300 fs_sm white Lorem_Epsum_p_w341'>
                             .  Maecenas sed cras feugiat ligula auctor gravida. Nunc nisi massa scelerisque est.
                         </p>
                     </div>
                 </div>
-                <div>
-                    <div className=' d-flex'>
+                <div className=' d-flex align-items-end pt-5'>
+
+                    <div className=' d-flex '>
                         <img src={Manufacturing_Lorem_img2} alt="image" />
                         <div className=' ps-4'>
-                            <p className=' ff_Book fw_300 fs_sm white Lorem_Epsum_p_w pe-5 me-5'>
+                            <p className=' ff_Book fw_300 fs_sm white Lorem_Epsum_p_w284'>
                                 .  Volutpat ipsum nunc non e molestie  swssa volutpat mauris.
                             </p>
-                            <p className=' ff_Book fw_300 fs_sm white Lorem_Epsum_p_w w-50'>
+                            <p className=' ff_Book fw_300 fs_sm white Lorem_Epsum_p_w284 pe-5'>
                                 .  Volutpat ipsum nunc non e molestie  swssa.
                             </p>
                         </div>
+                    </div>
+                    <div>
+                        <img src={Manufacturing_Lorem_img3} alt="image" />
                     </div>
                 </div>
 
@@ -42,4 +46,4 @@ const Lorem_Epsum = () => {
     )
 }
 
-export default Lorem_Epsum
+export default Lorem_Epsum  
