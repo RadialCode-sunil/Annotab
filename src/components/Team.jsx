35565b6917efd88e0e84da5bd7e91ecd05e8a355
RadialCode-sunil <sunilbatesar0@gmail.com>
2023-04-12 19:42:58 +0530

    Green_btn_2 add

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,14 +3,13 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Slider from "react-slick";
 import Team_boy_img_1 from '../assets/img/png/Team_boy_img_1.png';
 import Team_boy_img_2 from '../assets/img/png/Team_boy_img_2.png';
-import Team_boy_img_3 from '../assets/img/png/Team_boy_img_3.png';
-import green_light from '../assets/img/png/green_light.png'
+import Team_boy_img_3 from '../assets/img/png/Team_boy_img_3.png';  
 const Team = () => {
     var Team_slider = {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 2.18,
+        slidesToShow: 2.22,
         slidesToScroll: 1,
         arrows: false,
         centerMode: true,
@@ -28,7 +27,7 @@ const Team = () => {
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 1.25,
+                    slidesToShow: 1.22,
                     slidesToScroll: 2,
                     dots: false,
                 }
@@ -46,16 +45,16 @@ const Team = () => {
     };
     return (
         <section className='py-5 position-relative overflow-hidden'>
-            <span className='Team_green_light'></span>
+            <div className='Team_green_light position-absolute'></div>
             <Container>
                 <h2 className=' ff_Medium fw_500 fs_3xl white text-center'>Team</h2>
                 <p className=' ff_Book fw_300 fs_sm white text-center Team_p_w mx-auto pt-3'>
                     Risus suspendisse mauris sit placerat et interdum. Mauris tellus et visssg. Urna diam massa varius orci porta.
                 </p>
                 <div className='Team_slider_visible'>
-                    <Slider {...Team_slider} className='pt-5 '>
-                        <div>
-                            <div className='Team_slider_box position-relative mx-3'>
+                    <Slider {...Team_slider} className='pt-5'>
+                        <div className=' py-1'>
+                            <div className='Team_slider_box position-relative mx-3 overflow-hidden'>
                                 <img className=' w-100 Team_slider_box_img' src={Team_boy_img_2} alt="Boy" />
                                 <div className='Team_slider_img_box position-absolute'>
                                     <p className=' ff_Medium fw_500 fs_lg white'>Et urna nisi lorem</p>
@@ -65,8 +64,8 @@ const Team = () => {
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <div className='Team_slider_box position-relative mx-3'>
+                        <div className=' py-1'>
+                            <div className='Team_slider_box position-relative mx-3 overflow-hidden'>
                                 <img className=' w-100 Team_slider_box_img' src={Team_boy_img_3} alt="Boy" />
                                 <div className='Team_slider_img_box position-absolute'>
                                     <p className=' ff_Medium fw_500 fs_lg white'>Et urna nisi lorem</p>
@@ -76,8 +75,8 @@ const Team = () => {
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <div className='Team_slider_box position-relative mx-3'>
+                        <div className=' py-1'>
+                            <div className='Team_slider_box position-relative mx-3 overflow-hidden'>
                                 <img className=' w-100 Team_slider_box_img' src={Team_boy_img_1} alt="Boy" />
                                 <div className='Team_slider_img_box position-absolute'>
                                     <p className=' ff_Medium fw_500 fs_lg white'>Et urna nisi lorem</p>
