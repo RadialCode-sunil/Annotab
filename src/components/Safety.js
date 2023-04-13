a7adf0a4fb48e14c30ade222cb5aed5b6526971b
Ankit-191 <godaraankit003>
2023-04-13 13:38:57 +0530

    commit

--- src/components/Safety.js
+++ src/components/Safety.js
@@ -9,7 +9,8 @@ import Footer from './Footer'
 import Testimonials from './Testimonials'
 const Safety = () => {
   return (
-    <div><Hero />
+    <div>
+      <Hero />
       <Testimonials />
       <Turpis />
       <Footer /></div>
