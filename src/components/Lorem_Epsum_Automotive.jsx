376c4480f584eba2151aff5d21aa0c0c11b8b0ce
RadialCode-sunil <sunilbatesar0@gmail.com>
2023-04-14 00:06:49 +0530

    Lorem_Epsum_Automotive

--- src/components/Lorem_Epsum_Automotive.jsx
+++ src/components/Lorem_Epsum_Automotive.jsx
@@ -9,7 +9,7 @@ const Lorem_Epsum_Automotive = (props) => {
                     Lorem Epsum Dolar Sit emit
                 </h2>
 
-                <div className=' d-flex flex-column-reverse flex-lg-row justify-content-between pt-5'>
+                <div className=' d-flex flex-column-reverse flex-lg-row justify-content-between align_items_center_xs pt-5'>
 
                     <div className='py-4 py-lg-0'>
                         <ul className=' list-unstyled ms-lg-4 ps-lg-2'>
@@ -17,37 +17,41 @@ const Lorem_Epsum_Automotive = (props) => {
                                 Volutpat ipsum nunc non e molestie  swssa volutpat mauris.
                             </li>
 
-                            <li className=' ff_Book fw_300 fs_sm white opacity_7 Lorem_Epsum_p_w284 pe-3  position-relative list_dot pt-3 Volutpat_p_2 position-relative'>
+                            <li className=' ff_Book fw_300 fs_sm white opacity_7 Lorem_Epsum_p_w284 pe-3  position-relative list_dot pt-3 Volutpat_p_2 position-relative mb-5 mb-sm-0 pb-5 pb-sm-0'>
                                 Volutpat ipsum nunc non e molestie  swssa.
                             </li>
                         </ul>
                     </div>
-                    <div className='Lorem_Epsum_Automotive_img_1  position-relative d-inline me-5 pe-4'><img className=' img-fluid img_w' src={props.Lorem_Epsum_img1} alt="Data engine" /></div>
+                    <div className='Lorem_Epsum_Automotive_img_1  position-relative d-inline me-sm-5 pe-sm-4 mb-5 mb-sm-0 pb-5 pb-sm-0'>
+                        <img className=' img-fluid img_w' src={props.Lorem_Epsum_img1} alt="Data engine" />
+                    </div>
                 </div>
 
-                <div className='d-flex flex-column flex-lg-row justify-content-between Lorem_Epsum_Automotive_img_2 position-relative d-inline'>
-                    <img className=' img-fluid img2_w' src={props.Lorem_Epsum_img2} alt="Data engine" />
-                    <div className=' py-4 py-lg-0 pt-lg-5 mt-lg-3'>
+                <div className='d-flex flex-column flex-lg-row justify-content-between align_items_center_xs Lorem_Epsum_Automotive_img_2 position-relative d-inline pb-4 pb-sm-0'>
+                    <img className=' img-fluid img2_w pb-4 pb-sm-0' src={props.Lorem_Epsum_img2} alt="Data engine" />
+                    <div className=' my-5 my-sm-0 py-4 py-lg-0 pt-lg-5 mt-lg-3'>
                         <ul className='Amet_ul list-unstyled'>
                             <li className=' ff_Book fw_300 fs_sm white opacity_7 Lorem_Epsum_p_w284 position-relative pt-3 pt-lg-0 list_dot Amet_p position-relative'>
                                 Amet at ultricies adipiscing lectus sed vitae. Nullam quam facilisi.
                             </li>
 
-                            <li className=' ff_Book fw_300 fs_sm white opacity_7 Lorem_Epsum_p_w341 position-relative list_dot pt-2 '>
+                            <li className=' ff_Book fw_300 fs_sm white opacity_7 Lorem_Epsum_p_w341 position-relative list_dot pt-2 Amet_p_div'>
                                 Maecenas sed cras feugiat ligula auctor gravida. Nunc nisi massa scelerisque est.
                             </li>
                         </ul>
                     </div>
                 </div>
-                <div className='d-flex flex-column-reverse flex-lg-row justify-content-between align-items-lg-end'>
+                <div className='d-flex flex-column-reverse flex-lg-row justify-content-between align_items_center_xs align-items-lg-end'>
 
-                    <div className=' pt-4 pt-lg-5 '>
+                    <div className=' mt-5 mt-sm-0 pt-4 pt-lg-5 '>
                         <ul className='Volutpat_ul list-unstyled ms-lg-4 ps-lg-2'>
-                            <li className=' ff_Book fw_300 fs_sm white opacity_7 Lorem_Epsum_p_w341 position-relative pt-3 pt-lg-0 list_dot'>
+                            <li className=' ff_Book fw_300 fs_sm white opacity_7 Lorem_Epsum_p_w341 position-relative pt-3 pt-lg-0 list_dot .Lorem_div_height {
+  height: 200px;
+}'>
                                 Volutpat ipsum nunc non e molestie  swssa volutpat mauris.
                             </li>
 
-                            <li className=' ff_Book fw_300 fs_sm white opacity_7 position-relative list_dot pt-2 '>
+                            <li className=' ff_Book fw_300 fs_sm white opacity_7 position-relative list_dot pt-2 Volutpat_ul_3'>
                                 Volutpat ipsum nunc non e molestie  swssa.
                             </li>
                             <li className=' ff_Book fw_300 fs_sm white opacity_7 position-relative list_dot pt-2 '>
@@ -55,7 +59,10 @@ const Lorem_Epsum_Automotive = (props) => {
                             </li>
                         </ul>
                     </div>
-                    <div className='Lorem_Epsum_Automotive_img_3 position-relative d-inline me-5 pe-4'><img className=' img-fluid img_w' src={props.Lorem_Epsum_img3} alt="Data engine" /></div>
+                    <div className='Lorem_Epsum_Automotive_img_3 position-relative d-inline me-sm-5 pe-sm-4 pb-3 pb-sm-0'>
+
+                        <img className=' img-fluid img_w' src={props.Lorem_Epsum_img3} alt="Data engine" />
+                    </div>
                 </div>
                 <div className=' Lorem_div_height'>
                 </div>
