f9acf277950589d7c73f020ea196d09c3688c541
Sanjay2929 <sanjayjyani220@gmail.com>
2023-04-13 13:38:11 +0530

    commit

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -40,56 +40,79 @@ const Solutions = () => {
     };
   return (
     <div>
-        <Container/>
-        <Row/>
-        <Col/>
-        <section className='bg_darkblue py-5 overflow-hidden'>
-            <Container>
-                <div className='d-lg-flex text-center text-lg-start justify-content-between align-items-center pb-5'>
-                    <div>
-                        <p className='ff_Medium fs_3xl grey'>Solutions</p>
-                        <p className='ff_Book fs_sm white opacity_7'>Sit diam dictumst est pellentesque nunc mattis. Ut.</p>
-                    </div>
-                    <div className='d-flex justify-content-center text-center align-items-center'>
-                        <div className='line me-3'> </div>
-                        <p className='mb-0 ff_Book fs_sm white opacity_7'>Slide for more</p>
-                        <div className='line ms-3'> </div>
-                    </div>
-                </div>
-            <div className='solutions pt-3'>
-                <Slider  {...Team_slider}>
-                <div className='px-2 h-100 d-flex flex-column justify-content-between'>
-                    <div><p className='ff_Medium fs_lg white'>Healthcare & Medicine</p>
-                    <p className='ff_Book fs_sm white opacity_7'>Tristique fames neque facilisis bibendum pretium elementum non. commodo. Cursus suspendisse vel quis ridiculus id. Volutpat vulputate mollis at sit.</p></div>
-                    <Green_btn any_text='Learn More'/>
-                </div>
-                <div className='px-2 h-100 d-flex flex-column justify-content-between'>
-                    <div>
-                        <p className='ff_Medium fs_lg white'>Manufacturing</p>
-                        <p className='ff_Book fs_sm white opacity_7'>Felis nunc senectus integer nisi eget nunc. Convallis semper eget metus ullamcorper adipiscing proin ultrices massa convallis tortor aliquet.</p>
-                    </div>                       
-                    <Green_btn any_text='Learn More'/>
-                </div>
-                <div className='px-2 h-100 d-flex flex-column justify-content-between'>
-                    <div>
-                        <p className='ff_Medium fs_lg white'>Agriculture</p>
-                    <p className='ff_Book fs_sm white opacity_7'>Pellentesque massa eu at adipiscing quam viverra ut lectus. Bibendum dis donec a diam. Dui lacus tortor dignissim vel est vestibulum augue sagittis in egestas.</p>
-                    </div>
-                    <Green_btn any_text='Learn More'/>
-                </div>
-                <div className='px-2 h-100 d-flex flex-column justify-content-between'>
-                    <div>
-                        <p className='ff_Medium fs_lg white'>Aerospace & Defense</p>
-                    <p className='ff_Book fs_sm white opacity_7'>Pellentesque massa eu at adipiscing quam viverra ut lectus. Bibendum dis donec a diam. Dui lacus tortor dignissim vel est vestibulum augue sagittis in egestas.</p>
-                    </div>
-                    <Green_btn any_text='Learn More'/>
-                </div>
-                </Slider>
+      <Container />
+      <Row />
+      <Col />
+      <section className="bg_darkblue py-5 overflow-hidden ">
+        <Container>
+          <div className="d-lg-flex text-center text-lg-start justify-content-between align-items-center pb-5">
+            <div>
+              <p className="ff_Medium fs_3xl grey">Solutions</p>
+              <p className="ff_Book fs_sm white opacity_7">
+                Sit diam dictumst est pellentesque nunc mattis. Ut.
+              </p>
             </div>
-            </Container>
-        </section>
+            <div className="d-flex justify-content-center text-center align-items-center">
+              <div className="line me-3"> </div>
+              <p className="mb-0 ff_Book fs_sm white opacity_7">
+                Slide for more
+              </p>
+              <div className="line ms-3"> </div>
+            </div>
+          </div>
+          <div className="solutions pt-3">
+            <Slider {...Team_slider}>
+              <div className="px-2 h-100 d-flex flex-column justify-content-between">
+                <div>
+                  <p className="ff_Medium fs_lg white">Healthcare & Medicine</p>
+                  <p className="ff_Book fs_sm white opacity_7">
+                    Tristique fames neque facilisis bibendum pretium elementum
+                    non. commodo. Cursus suspendisse vel quis ridiculus id.
+                    Volutpat vulputate mollis at sit.
+                  </p>
+                </div>
+                <Green_btn any_text="Learn More" />
+              </div>
+              <div className="px-2 h-100 d-flex flex-column justify-content-between">
+                <div>
+                  <p className="ff_Medium fs_lg white">Manufacturing</p>
+                  <p className="ff_Book fs_sm white opacity_7">
+                    Felis nunc senectus integer nisi eget nunc. Convallis semper
+                    eget metus ullamcorper adipiscing proin ultrices massa
+                    convallis tortor aliquet.
+                  </p>
+                </div>
+                <Green_btn any_text="Learn More" />
+              </div>
+              <div className="px-2 h-100 d-flex flex-column justify-content-between">
+                <div>
+                  <p className="ff_Medium fs_lg white">Agriculture</p>
+                  <p className="ff_Book fs_sm white opacity_7">
+                    Pellentesque massa eu at adipiscing quam viverra ut lectus.
+                    Bibendum dis donec a diam. Dui lacus tortor dignissim vel
+                    est vestibulum augue sagittis in egestas.
+                  </p>
+                </div>
+                <Green_btn any_text="Learn More" />
+              </div>
+              <div className="px-2 h-100 d-flex flex-column justify-content-between">
+                <div>
+                  <p className="ff_Medium fs_lg white">Aerospace & Defense</p>
+                  <p className="ff_Book fs_sm white opacity_7">
+                    Pellentesque massa eu at adipiscing quam viverra ut lectus.
+                    Bibendum dis donec a diam. Dui lacus tortor dignissim vel
+                    est vestibulum augue sagittis in egestas.
+                  </p>
+                </div>
+                <Green_btn any_text="Learn More" />
+              </div>
+            </Slider>
+          </div>
+        </Container>
+       
+      </section>
     </div>
-  )
+  );
 }
 
 export default Solutions
