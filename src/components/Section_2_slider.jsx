c31bb1e9400d3c6081af95c32a2d330fafbdaff6
sunil-170 <sunilkhokha015@gmail.com>
2023-04-13 15:32:36 +0530

    slider_gap

--- src/components/Section_2_slider.jsx
+++ src/components/Section_2_slider.jsx
@@ -19,7 +19,7 @@ const Section_2_slider = () => {
             {
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 3.25,
+                    slidesToShow: 7.25,
                     slidesToScroll: 3,
                     infinite: true,
                     dots: false
@@ -28,14 +28,14 @@ const Section_2_slider = () => {
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 2.25,
+                    slidesToShow: 7.25,
                     slidesToScroll: 2
                 }
             },
             {
                 breakpoint: 670,
                 settings: {
-                    slidesToShow: 1.03,
+                    slidesToShow: 4.03,
                     slidesToScroll: 1
                 }
             }]
