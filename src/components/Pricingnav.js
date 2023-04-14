d463427c984bf3577a21bfc46eaf66238cf7cd09
RadialCode-sunil <sunilbatesar0@gmail.com>
2023-04-14 12:14:18 +0530

    commit

--- src/components/Pricingnav.js
+++ src/components/Pricingnav.js
@@ -11,7 +11,6 @@ import Pricingplan from './Pricingplan'
 const Pricingnav = () => {
     return (
         <div>
-            <Hero />
             <Pricingplan/>
             <Latest />
             <Testimonials />
