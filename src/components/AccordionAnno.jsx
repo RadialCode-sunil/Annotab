68fe94c3c6ee1c37889e08008cc1557bef25aac2
vishal-jaat-165 <vjaat5597@gmail.com>
2023-04-14 16:40:09 +0530

    changes

--- src/components/AccordionAnno.jsx
+++ src/components/AccordionAnno.jsx
@@ -5,38 +5,38 @@ const AccordionAnno = () => {
     return (
         <div >
             <Accordion defaultActiveKey="0" flush>
-                <Accordion.Item eventKey="0" className='mb-2'>
-                    <Accordion.Header className=' '><p className='ff_Medium grey fs_xl fw_700 mb-0mb-0'>Annotab Studio</p></Accordion.Header>
+                <Accordion.Item eventKey="0" className='mb-2 '>
+                    <Accordion.Header className=' '><p className='ff_Medium grey fs_xl fw_700 mb-0 p-4'>Annotab Studio</p></Accordion.Header>
                     <Accordion.Body>
-                        <p className=' ff_Book  fs_sm grey  fw_300 max_width_438 pb-4 dot'>Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum. Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum.  proin eu vestibulum.</p>
-                        <div className=' mb-4'>
+                        <p className=' ff_Book  fs_sm grey  fw_300 max_width_438 pb-4 ps-4'>Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum. Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum.  proin eu vestibulum.</p>
+                        <div className=' mb-4 ps-4'>
                             <Green_btn any_text='Learn More' />
                         </div>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1" className='mb-2'>
-                    <Accordion.Header ><p className=' ff_Medium grey fs_xl fw_700 mb-0'>Video Annotab </p></Accordion.Header>
+                    <Accordion.Header ><p className=' ff_Medium grey fs_xl fw_700 mb-0 p-4'>Video Annotab </p></Accordion.Header>
                     <Accordion.Body>
-                        <p className=' ff_Book  fs_sm grey  fw_300 max_width_438 pb-5'>Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum. Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum.  proin eu vestibulum.</p>
-                        <div className=' mb-4'>
+                        <p className=' ff_Book  fs_sm grey  fw_300 max_width_438 pb-5 ps-4'>Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum. Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum.  proin eu vestibulum.</p>
+                        <div className=' mb-4 ps-4'>
                             <Green_btn any_text='Learn More' />
                         </div>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3" className='mb-2'>
-                    <Accordion.Header><p className=' ff_Medium grey fs_xl fw_700 mb-0'>Images Annotab </p></Accordion.Header>
+                    <Accordion.Header><p className=' ff_Medium grey fs_xl fw_700 mb-0 p-4'>Images Annotab </p></Accordion.Header>
                     <Accordion.Body>
-                        <p className=' ff_Book  fs_sm grey  fw_300 max_width_438 mb-5'>Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum. Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum.  proin eu vestibulum.</p>
-                        <div className=' mb-4'>
+                        <p className=' ff_Book  fs_sm grey  fw_300 max_width_438 mb-5 ps-4'>Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum. Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum.  proin eu vestibulum.</p>
+                        <div className=' mb-4 ps-4'>
                             <Green_btn any_text='Learn More' />
                         </div>
                     </Accordion.Body>
                 </Accordion.Item>
-                <Accordion.Item eventKey="4" className='mb-2'>
-                    <Accordion.Header><p className=' ff_Medium grey fs_xl fw_700 mb-0'>Annoations</p></Accordion.Header>
-                    <Accordion.Body>
-                        <p className=' ff_Book  fs_sm grey  fw_300 max_width_438 mb-5'>Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum. Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum.  proin eu vestibulum.</p>
-                        <div className=' mb-4'>
+                <Accordion.Item eventKey="4" className='mb-2 py-3 grey'>
+                    <Accordion.Header><p className=' ff_Medium grey fs_xl fw_700 mb-0 p-4'>Annoations</p></Accordion.Header>
+                    <Accordion.Body className='lh_25'>
+                        <p className=' ff_Book  fs_sm grey  fw_300 max_width_438 mb-5 ps-4'>Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum. Ultricies in integer semper pharetra. Facilisi neque nec id proin eu vestibulum.  proin eu vestibulum.</p>
+                        <div className=' mb-4 ps-4'>
                             <Green_btn any_text='Learn More' />
                         </div>
                     </Accordion.Body>
