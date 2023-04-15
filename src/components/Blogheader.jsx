353fb6db74e5bf0993a73da5575dc45654b4186f
Ankit-191 <godaraankit003>
2023-04-15 09:45:18 +0530

    commit

--- src/components/Blogheader.jsx
+++ src/components/Blogheader.jsx
@@ -12,7 +12,7 @@ const Blogheader = () => {
           <Container>
             <div className='green_light1 d-none d-md-block'></div>
             <div className='d-flex flex-column align-items-center justify-content-center h_454'>
-                <p className='ff_Bold fs_4xl white'>Blogs</p>
+                <p className='ff_Bold fs_4xl grey'>Blogs</p>
                 <p className='ff_Book fs_sm grey opacity_7 maxw_627'>Arcu nunc ut tristique massa proin bibendum et. At maecenas elementum vitae dictum pulvinar vitae. Pellentesque sem ipsum at facilisis lobortis justo. Feugiat.</p>
             </div>
             <div className='blue_light1 d-none d-md-block'></div>
