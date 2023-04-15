554bda496e97ddd3ec0e19c1b5f98b79d6f56ac0
Ankit-191 <godaraankit003>
2023-04-15 09:39:12 +0530

    commit

--- src/components/Pricingplan.jsx
+++ src/components/Pricingplan.jsx
@@ -10,7 +10,7 @@ const Pricingplan = () => {
         <Container>
             <div className='d-flex flex-column align-items-center justify-content-center h_454'>
                 <p className='ff_Bold fs_4xl white'>Pricing Plan</p>
-                <p className='ff_Bold fs_sm grey opacity_7 maxw_689 text-center'>Aliquam in cursus non massa adipiscing. Massa at vulputate pellentesque fringilla in. Est consequat eu sapien et facilisi in. Ac non nibh malesuada sed.</p>
+                <p className='ff_Book fs_sm grey opacity_7 maxw_689 text-center'>Aliquam in cursus non massa adipiscing. Massa at vulputate pellentesque fringilla in. Est consequat eu sapien et facilisi in. Ac non nibh malesuada sed.</p>
             </div>
         </Container>
       </div>
