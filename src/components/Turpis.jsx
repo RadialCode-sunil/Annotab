f0fe3c6ceae2308994c10725612ff7d723ab7b72
sunil-170 <sunilkhokha015@gmail.com>
2023-04-15 09:29:02 +0530

    clr

--- src/components/Turpis.jsx
+++ src/components/Turpis.jsx
@@ -7,11 +7,11 @@ export const Turpis = () => {
             <Container>
                 <div className='turpis_img text-center py-5 my-5'>
                     <h3 className=' ff_Medium fs_3xl fw_400 white mt-4'>Turpis id consectetur</h3>
-                    <p className=' ff_Book fs_sm fw_300 white feauth_wdth mx-auto pt-3 p-1 p-xl-0'>Feugiat tempor sagittis vitae amet dis nisl est. Neque tristique tellus
+                    <p className=' ff_Book fs_sm fw_300 grey feauth_wdth mx-auto mt-3 p-1 p-xl-0'>Feugiat tempor sagittis vitae amet dis nisl est. Neque tristique tellus
                         etiam nascetur at adipiscing. Tellus morbi faucibus at aliquam venenatis. Suspendis
                         se vitae sed os maecenas. Malesuada in tortor cras dapibus.</p>
                     <div className=' mt-5 mb-3'>
-                        <Green_btn any_text='Sign up for free' />
+                        <Green_btn any_text='Sign Up For Free' />
                     </div>
                 </div>
             </Container>
